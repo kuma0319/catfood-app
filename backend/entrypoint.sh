f8a4e34c4c4cb1e5e2529cf3c2c3148edf539399
kuma0319 <h.mukohara0319@gmail.com>
2023-06-22 13:21:47 +0900

    🔧 entrypoint.shを修正

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,9 +4,12 @@ set -e
 # Railsのserver.pidを削除(pidがあるとサーバー起動中と判断されるため)
 rm -f /app/tmp/pids/server.pid
 
-# 初期データの投入
-bundle exec rake db:migrate
-bundle exec rails db:seed
+# prodction環境のみ初期データの投入
+if [ "$RAILS_ENV" = "production" ]; then
+bundle exec rake db:reset
+# bundle exec rake db:migrate
+# bundle exec rails db:seed
+fi
 
 #コンテナのメインプロセスを実行
 exec "$@"
