58979bc8adfdde36424fcb68d46bbc876c635811
kuma0319 <h.mukohara0319@gmail.com>
2023-06-23 18:06:09 +0900

    🚧 新規登録、ログイン用フォーム作成

---
+++ frontend/src/pages/sign_in.tsx
@@ -0,0 +1,7 @@
+import SignInForm from "@/components/authentication/SignInForm";
+
+const signIn = () => {
+  return <SignInForm />;
+};
+
+export default signIn;
---
+++ frontend/src/pages/sign_up.tsx
@@ -0,0 +1,7 @@
+import SignUpForm from "@/components/authentication/SignUpForm";
+
+const signUp = () => {
+  return <SignUpForm />;
+};
+
+export default signUp;
