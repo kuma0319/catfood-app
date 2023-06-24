015668ae9b81013fbc12ee0ddd0601d28291bf82
kuma0319 <h.mukohara0319@gmail.com>
2023-06-25 02:17:11 +0900

    🚧 ログインコンポーネントを実装

--- frontend/src/pages/sign_in.tsx
+++ frontend/src/pages/sign_in.tsx
@@ -1,7 +1,43 @@
-import SignInForm from "@/components/authentication/SignInForm";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { setCookie } from "nookies";
+import { useState } from "react";
 
-const signIn = () => {
-  return <SignInForm />;
+import SignInForm from "@/components/authentication/SignInForm";
+import { authSignInUrl } from "@/urls";
+
+interface SignInInput {
+  email: string;
+  password: string;
+}
+
+const SignIn = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
+
+  const onSignIn = async (data: SignInInput) => {
+    const email = data.email;
+    const password = data.password;
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}${authSignInUrl}`,
+        { email, password },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      if (response.status === 200) {
+        setCookie(null, "uid", response.headers["uid"]);
+        setCookie(null, "client", response.headers["client"]);
+        setCookie(null, "access-token", response.headers["access-token"]);
+        // 認証成功時にホームページへリダイレクト
+        router.push("/");
+      }
+    } catch (error: any) {
+      // エラー発生時はエラーメッセージをセット
+      setErrorMessage(error.response.data.errors);
+    }
+  };
+
+  return <SignInForm onSignIn={onSignIn} errorMessage={errorMessage} />;
 };
 
-export default signIn;
+export default SignIn;
--- frontend/src/urls/index.ts
+++ frontend/src/urls/index.ts
@@ -9,3 +9,4 @@ export const foodSearchUrl = `/api/v1/foods/search.json`;
 export const watchListUrl = `/api/v1/foods/watch_lists.json`;
 
 export const authUrl = `/api/v1/auth`;
+export const authSignInUrl = `/api/v1/auth/sign_in`;
