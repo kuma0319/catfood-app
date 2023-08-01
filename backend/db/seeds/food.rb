d7fc8fa3eac130185bf0d72b22982a697449dcaf
kuma0319 <h.mukohara0319@gmail.com>
2023-08-02 05:27:48 +0900

    👔 楽天API用のロジック修正 🗃️ テーブル設計の修正

--- backend/db/seeds/food.rb
+++ backend/db/seeds/food.rb
@@ -9,8 +9,13 @@ CSV.foreach(ENV['FOOD_CSV_PATH'], headers: true) do |row|
     production_area_id: row[4],
     food_type_id: row[5],
     calorie: row[6],
-    rakuten_name: row[7],
-    medium_image_url: row[8],
-    min_price: row[9],
+    target_age:row[7],
+    rakuten_name: row[8],
+    rakuten_item_code: row[9],
+    medium_image_url: row[10],
+    min_price: row[11],
+    max_price: row[12],
+    median_price: row[13],
+    add_date: row[14],
   )
 end
