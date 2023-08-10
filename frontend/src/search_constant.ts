b3353800ef1e08ec10fe899a1cca849e62b535e2
kuma0319 <h.mukohara0319@gmail.com>
2023-08-11 03:34:09 +0900

    💄 フード詳細ページのUI更新

--- frontend/src/search_constant.ts
+++ frontend/src/search_constant.ts
@@ -122,107 +122,6 @@ export const AMOUNT_RANGE = [0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0];
 //成分含有量
 export const NUTRIENT_CONTENT_RANGE = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
-//範囲検索用のパラメータ定数
-export const RANGE_OPTION_PARAMS = [
-  {
-    label: "カロリー",
-    max_name: "max_calorie",
-    min_name: "min_calorie",
-    range: CALORIE_RANGE,
-    unit: "kcal/100g",
-  },
-  {
-    label: "金額",
-    max_name: "max_price",
-    min_name: "min_price",
-    range: PRICE_RANGE,
-    unit: "円",
-  },
-  {
-    label: "内容量",
-    max_name: "max_amount",
-    min_name: "min_amount",
-    range: AMOUNT_RANGE,
-    unit: "kg",
-  },
-  {
-    label: "タンパク質",
-    max_name: "max_protein_content",
-    min_name: "min_protein_content",
-    range: NUTRIENT_CONTENT_RANGE,
-    unit: "%",
-  },
-  {
-    label: "脂質",
-    max_name: "max_fat_content",
-    min_name: "min_fat_content",
-    range: NUTRIENT_CONTENT_RANGE,
-    unit: "%",
-  },
-  {
-    label: "粗繊維",
-    max_name: "max_fibre_content",
-    min_name: "min_fibre_content",
-    range: NUTRIENT_CONTENT_RANGE,
-    unit: "%",
-  },
-  {
-    label: "灰分",
-    max_name: "max_ash_content",
-    min_name: "min_ash_content",
-    range: NUTRIENT_CONTENT_RANGE,
-    unit: "%",
-  },
-  {
-    label: "水分",
-    max_name: "max_moisture_content",
-    min_name: "min_moisture_content",
-    range: NUTRIENT_CONTENT_RANGE,
-    unit: "%",
-  },
-];
-
-//キーワード検索のkeyWords用の型エラー用にnameの型を限定
-type KeyName =
-  | "food_name"
-  | "not_food_name"
-  | "ingredients"
-  | "not_ingredients";
-
-//キーワード検索用のパラメータ定数
-export const FOOD_SEARCH_INPUT_PARAMS: Array<{
-  name: KeyName;
-  label: string;
-  placeholder: string;
-}> = [
-  {
-    name: "food_name",
-    label: "このキーワードを「名前」に含める",
-    placeholder: "名前1 名前2...",
-  },
-  {
-    name: "not_food_name",
-    label: 'このキーワードを「名前」に"含めない"',
-    placeholder: "名前1 名前2...",
-  },
-  {
-    name: "ingredients",
-    label: "このキーワードを「原材料」に含める",
-    placeholder: "原材料1 原材料2...",
-  },
-  {
-    name: "not_ingredients",
-    label: 'このキーワードを「原材料」に"含めない"',
-    placeholder: "原材料1 原材料2...",
-  },
-];
-
-//複数一致検索用のパラメータ定数
-export const MULTI_OPTION_PARAMS = [
-  { name: "brand_id", items: BRAND, label: "ブランド" },
-  { name: "production_area_id", items: PRODUCTION_AREA, label: "産地" },
-];
-
 export const SEARCH_BRAND_PARAMS = [
   { name: "brand_id", items: BRAND, label: "ブランド" },
 ];
@@ -290,5 +189,40 @@ export const SEARCH_AMOUNT_PARAMS = [
     min_name: "min_amount",
     range: AMOUNT_RANGE,
     unit: "kg",
+  },
+];
+
+//キーワード検索のkeyWords用の型エラー用にnameの型を限定
+type KeyName =
+  | "food_name"
+  | "not_food_name"
+  | "ingredients"
+  | "not_ingredients";
+
+//キーワード検索用のパラメータ定数
+export const FOOD_SEARCH_INPUT_PARAMS: Array<{
+  name: KeyName;
+  label: string;
+  placeholder: string;
+}> = [
+  {
+    name: "food_name",
+    label: "このキーワードを「名前」に含める",
+    placeholder: "名前1 名前2...",
+  },
+  {
+    name: "not_food_name",
+    label: 'このキーワードを「名前」に"含めない"',
+    placeholder: "名前1 名前2...",
+  },
+  {
+    name: "ingredients",
+    label: "このキーワードを「原材料」に含める",
+    placeholder: "原材料1 原材料2...",
+  },
+  {
+    name: "not_ingredients",
+    label: 'このキーワードを「原材料」に"含めない"',
+    placeholder: "原材料1 原材料2...",
   },
 ];
