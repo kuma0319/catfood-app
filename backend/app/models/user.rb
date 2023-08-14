80d5fd0054b6909cec4ce559678d5c647a986362
kuma0319 <h.mukohara0319@gmail.com>
2023-08-14 13:34:11 +0900

    🗃️ Userのマイグレーション更新、jbuilderの形式を一部変更

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -19,6 +19,9 @@ class User < ApplicationRecord
   has_many :questions
   has_many :answers
 
+  # ニックネームに追加のバリデーション
+  validates :nickname, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+
   # avatarのURLを返すメソッド
   def avatar_url
     avatar.attached? ? url_for(avatar) : nil
