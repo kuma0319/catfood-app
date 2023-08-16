f687323d7541f89433e4462b971f20d65fffd693
kuma0319 <h.mukohara0319@gmail.com>
2023-08-16 21:40:18 +0900

    🌱 データ更新可能な形式にロジック修正

--- backend/db/seeds/nutrient_content.rb
+++ backend/db/seeds/nutrient_content.rb
@@ -17,8 +17,9 @@ CSV.parse(csv_data, headers: true) do |row|
     next
   end
 
-  NutrientContent.create(
-    food_id: food.id,
+  # find_or_initialize_by(一致するレコードが無ければ新規作成、※createじゃないから保存はしない)でnameカラムに一致するレコードを見つける
+  nutrient_content = NutrientContent.find_or_initialize_by(food_id: food.id)
+  nutrient_content.update(
     nutrient_id: nutrient.id,
     content: row['content']
   )
