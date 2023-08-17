f4e9de4c408534fd5f7a547a7b0de09a0e561dd9
kuma0319 <h.mukohara0319@gmail.com>
2023-08-17 19:12:23 +0900

    📈 Googleアナリティクスを導入

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 
+import { GoogleAnalytics } from "@/components/commons/GoogleAnalytics";
 import { WatchListProvider } from "@/context/WatchListContext";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -14,6 +15,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     // ウォッチリストを共通化する用のcontext
     <WatchListProvider>
+      {/* Googleアナリティクス用 */}
+      <GoogleAnalytics />
       <Component {...pageProps} />
     </WatchListProvider>
   );
