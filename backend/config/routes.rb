f8cbced8c576037c88ced8e6acd275b5201082fd
kuma0319 <h.mukohara0319@gmail.com>
2023-08-18 12:42:51 +0900

    🔥 不要なAPIエンドポイントを削除

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace "api" do
     namespace "v1" do
       namespace "foods" do
-        resources :amounts, :brands, :food_types, :foods, :nutrient_contents, :nutrients, :production_areas
+        resources :foods
         get 'index_ids', to: 'foods#index_ids'
         get "search", to: "foods#search"
         get "watch_lists", to: "watch_lists#index"
